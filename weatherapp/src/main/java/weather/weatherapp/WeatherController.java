0febb7ae3b7ae99d39f46d7440a87a631de79282
Gourav Khore <gouravkhore656@gmail.com>
2026-05-11 14:18:16 +0530

    Updated WeatherController

--- weatherapp/src/main/java/weather/weatherapp/WeatherController.java
+++ weatherapp/src/main/java/weather/weatherapp/WeatherController.java
@@ -11,24 +11,34 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherController {
 
     private final RestTemplate restTemplate = new RestTemplate();
+
     private final String apikey = "6baba3bd162ce04b0cf27553c5f3f1a7";
 
     @GetMapping("/weather")
-    public String getWeather(@RequestParam String city) {
+    public ResponseEntity<String> getWeather(@RequestParam String city) {
+
         try {
 
-            String apikey = "6baba3bd162ce04b0cf27553c5f3f1a7";
-
             String url = "https://api.openweathermap.org/data/2.5/weather?q="
-                    + city.trim() + "&appid=" + apikey + "&units=metric";
+                    + city.trim()
+                    + "&appid=" + apikey
+                    + "&units=metric";
 
             String response = restTemplate.getForObject(url, String.class);
+
             return ResponseEntity.ok(response);
-            
+
         } catch (HttpClientErrorException e) {
-            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
+
+            return ResponseEntity
+                    .status(e.getStatusCode())
+                    .body(e.getResponseBodyAsString());
+
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("{\"message\": \"Internal Server Error: " + e.getMessage() + "\"}");
+
+            return ResponseEntity
+                    .status(500)
+                    .body("{\"message\":\"Internal Server Error\"}");
         }
     }
 }
